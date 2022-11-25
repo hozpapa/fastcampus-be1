6a5e554fa4745016179b3a3149905f13ec940dc2
echoiing <qwertyuiop7823@gmail.com>
2022-11-25 18:40:53 +0900

    feat: add code

--- src/me/day15/smartstore/customers/Customers.java
+++ src/me/day15/smartstore/customers/Customers.java
@@ -1,7 +1,5 @@
 package me.day15.smartstore.customers;
 
-import java.util.Arrays;
-
 public class Customers {
 
 }
--- src/me/day15/smartstore/util/Message.java
+++ src/me/day15/smartstore/util/Message.java
@@ -4,8 +4,7 @@ public class Message {
 
     public static final String ERR_MSG_INVALID_INPUT_NULL = "Null input. Please input something.";
     public static final String ERR_MSG_INVALID_INPUT_EMPTY = "Empty input. Please input something.";
-
-    public static final String ERR_MSG_INVALID_INPUT_RANGE = "Invalid input. Please try again.";
+    public static final String ERR_MSG_INVALID_INPUT_RANGE = "Invalid range for input. Please try again.";
     public static final String ERR_MSG_INVALID_INPUT_TYPE = "Invalid type for input. Please try again.";
     public static final String ERR_MSG_INVALID_INPUT_FORMAT = "Invalid format for input. Please try again.";
     public static final String END_MSG = "END";
