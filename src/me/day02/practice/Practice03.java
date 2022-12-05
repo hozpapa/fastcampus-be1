99dd58ba1d32124a8c00bd040a1528ad03b35c77
echoiing <qwertyuiop7823@gmail.com>
2022-12-06 01:43:03 +0900

    feat: add code

---
+++ src/me/day02/practice/Practice03.java
@@ -0,0 +1,10 @@
+package me.day02.practice;
+
+public class Practice03 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a); // variable
+        System.out.println('a'); // character
+        System.out.println("a"); // String
+    }
+}
