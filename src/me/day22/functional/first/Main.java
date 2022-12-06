02d57261bf96aa70c1be827e8e5b500b621cb560
echoiing <qwertyuiop7823@gmail.com>
2022-12-06 18:50:03 +0900

    feat: add code

--- src/me/day22/functional/first/Main.java
+++ src/me/day22/functional/first/Main.java
@@ -2,6 +2,16 @@ package me.day22.functional.first;
 
 public class Main {
     public static void main(String[] args) {
+//         Func func1 = a -> System.out.println(a); // 람다식 표현
+//         Func func2 = new Func() { // 익명 객체 표현
+//            @Override
+//            void method(Object a) {
+//                System.out.println(a);
+//            }
+//         }
+//         func2.method(3);
+
+
         Func func = s -> System.out.println(s); // 함수를 변수로 저장
         func.method("this is functional interface");
     }
